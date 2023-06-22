36ea2022965f5b3ec0d60e2e1e2c9d09c89f24e5
Aaron <iaaron.xyz@outlook.com>
2023-06-22 00:22:44 -0600

    create object for header data

--- src/components/RenderSection.js
+++ src/components/RenderSection.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
+import { headerData } from './cvData';
 
 
 class RenderSection extends Component {
   render() {
+
+    const {name, description, titleCareer} = headerData;
     return (
       <div className="render-section bg-white">
+
+        {/* HEADER */}
         <div id="header" className="bg-cyan-900 text-white">
           <div className="p-4">
-            <h1 className="text-4xl">Name</h1>
-            <h3 className="text-xl pb-2">Position/Career</h3>
-            <p>Description</p>
+            <h1 className="text-4xl" id="header-name">
+              {name}
+            </h1>
+            <h3 className="text-xl pb-2" id="header-description">
+              {titleCareer}
+            </h3>
+            <p>
+              {description}
+            </p>
           </div>
           <div className="bg-cyan-950 py-2">
             <ul className="list-none list-inline text-center">
@@ -22,6 +33,7 @@ class RenderSection extends Component {
             </ul>
           </div>
         </div>
+
       </div>
     )
   }
---
+++ src/components/cvData.js
@@ -0,0 +1,12 @@
+export const headerData = {
+  name : "Your name",
+  description : "Describe your field of study, your skills and your why.",
+  titleCareer: "Physics Graduate",
+  contactInfo : {
+    email : "",
+    number : "",
+    linkedin : "",
+    github : "",
+    website : "",
+  },
+}
