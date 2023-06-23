34d4a2c00616ceaafb90eb8c01d13f9b28686656
Aaron <iaaron.xyz@outlook.com>
2023-06-23 00:02:27 -0600

    add missing header elements

--- src/components/HeaderForm.js
+++ src/components/HeaderForm.js
@@ -4,6 +4,17 @@ const HeaderForm = ({ data, setData }) => {
     setData({...data, [e.target.name]: e.target.value})
   }
 
+  const handleContactInfo = (e) => {
+    console.log(data.contactInfo);
+    setData({
+      ...data,
+      contactInfo: {
+        ...data.contactInfo,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
   return (
     <div className="p-4 header-form-info">
       <h2 className="text-2xl">Header info</h2>
@@ -22,6 +33,19 @@ const HeaderForm = ({ data, setData }) => {
           />
         </div>
         <div className="form-element">
+          <label htmlFor="titleCareer">
+            Title
+          </label>
+          <input
+            type="text"
+            name="titleCareer"
+            id="title"
+            className="block w-full"
+            value={data.titleCareer}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-element">
           <label htmlFor="description">
             Description:
           </label>
@@ -32,6 +56,58 @@ const HeaderForm = ({ data, setData }) => {
             value={data.description}
             onChange={handleInputChange}>
           </textarea>
+        </div>
+
+        {/* Contact info */}
+        <div className="form-element">
+          <h2 className="text-xl">Contact Information</h2>
+          <div className="grid grid-cols-2 gap-4">
+            {/* email */}
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full"
+              value={data.contactInfo.email}
+              onChange={handleContactInfo}
+            />
+            {/* number phone */}
+            <input
+              type="text"
+              name="number"
+              id="number"
+              className="block w-full"
+              value={data.contactInfo.number}
+              onChange={handleContactInfo}
+            />
+            {/* linkedin */}
+            <input
+              type="url"
+              name="linkedin"
+              id="linkedin"
+              className="block w-full"
+              value={data.contactInfo.linkedin}
+              onChange={handleInputChange}
+            />
+            {/* github */}
+            <input
+              type="url"
+              name="github"
+              id="github"
+              className="block w-full"
+              value={data.contactInfo.github}
+              onChange={handleInputChange}
+            />
+            {/* website */}
+            <input
+              type="url"
+              name="website"
+              id="website"
+              className="block w-full"
+              value={data.contactInfo.website}
+              onChange={handleInputChange}
+            />
+          </div>
         </div>
       </form>
     </div>
