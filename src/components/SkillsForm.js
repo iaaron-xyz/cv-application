5f5cf61bb00f6dc10d3cfeec68825301671d142e
Aaron <iaaron.xyz@outlook.com>
2023-06-25 01:06:06 -0600

    use map index as id number

--- src/components/SkillsForm.js
+++ src/components/SkillsForm.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
 const SkillsForm = ({ data, setData }) => {
-
-  const [categoryCounter, setCategoryCounter] = useState(1);
 
   const handleInputCategory = (e) => {
     const categoryId = parseInt(e.target.id.split("-").pop());
     const updatedSkill = {
-      ...data.skills[categoryId-1],
+      ...data.skills[categoryId],
       skillCategory: e.target.value,
     };
     setData({...data, skills:[updatedSkill]});
@@ -18,7 +16,7 @@ const SkillsForm = ({ data, setData }) => {
     const skillsList = e.target.value.split(",");
     // Update list of skills
     const updatedList = {
-      ...data.skills[categoryId-1],
+      ...data.skills[categoryId],
       skillsList: skillsList,
     };
     setData({...data, skills: [updatedList]})
@@ -29,17 +27,17 @@ const SkillsForm = ({ data, setData }) => {
     <div className='p-4'>
       <h2 className='text-2xl'>Skills</h2>
       <form>
-        {data.skills.map((skill) => {
+        {data.skills.map((skill, index) => {
           return (
-            <div key={categoryCounter}>
+            <div key={index}>
               <div>
-                <label htmlFor={`skillsCategory-${categoryCounter}`}>
+                <label htmlFor={`skillsCategory-${index}`}>
                   Category
                 </label>
                 <input
                   type="text"
-                  name={`skillsCategory-${categoryCounter}`}
-                  id={`skillsCategory-${categoryCounter}`}
+                  name={`skillsCategory-${index}`}
+                  id={`skillsCategory-${index}`}
                   className="block w-full"
                   value={skill.skillCategory}
                   onChange={handleInputCategory}
@@ -47,20 +45,20 @@ const SkillsForm = ({ data, setData }) => {
               </div>
 
               <div>
-                <label htmlFor={`skillsList-${categoryCounter}`}>
+                <label htmlFor={`skillsList-${index}`}>
                   List of Skills
                 </label>
                 <input
                   type="text"
-                  name={`skillsList-${categoryCounter}`}
-                  id={`skillsList-${categoryCounter}`}
+                  name={`skillsList-${index}`}
+                  id={`skillsList-${index}`}
                   className="block w-full"
                   value={skill.skillsList}
                   onChange={handleInputSkillsList}
                 />
               </div>
               <div className='flex flex-row-reverse py-2'>
-                <button className='delete-btn' id={`remove-category-${categoryCounter}`}>Remove {skill.skillCategory} category</button>
+                <button className='delete-btn' id={`remove-category-${index}`}>Remove {skill.skillCategory} category</button>
               </div>
             </div>
           )
