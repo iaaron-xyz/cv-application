d3424734ba10a77d4d1597d47607c0b37456e113
Aaron <iaaron.xyz@outlook.com>
2023-06-26 16:01:15 -0600

    handle remove language form

--- src/components/LangFormSection.js
+++ src/components/LangFormSection.js
@@ -23,12 +23,21 @@ const LangFormSection = ({ data, setData }) => {
     });
   }
 
+  const handleRemoveLang = (langName) => {
+    // new list with removed language
+    let updatedLanguages = data.languages.filter((lang) => langName !== lang.language);
+    // update data
+    setData({...data, languages: updatedLanguages});
+  }
+
   return (
+    
     <div className='p-4'>
       <h2 className='text-2xl'>Languages</h2>
       <form>
         {data.languages.map((lang, index) => {
           return (
+            <>
             <div key={index} className='grid grid-cols-2 gap-1'>
               <div className='form-element'>
                 <label>
@@ -36,6 +45,7 @@ const LangFormSection = ({ data, setData }) => {
                 </label>
                 <input
                   type="text"
+                  name={`language-${index}`}
                   id={`language-${index}`}
                   className='block w-full'
                   value={lang.language}
@@ -48,6 +58,7 @@ const LangFormSection = ({ data, setData }) => {
               </label>
               <input
                 type="text"
+                name={`level-${index}`}
                 id={`level-${index}`}
                 className='block w-full'
                 value={lang.level}
@@ -55,6 +66,12 @@ const LangFormSection = ({ data, setData }) => {
               />
             </div>
           </div>
+            <div className=' flex flex-row-reverse pb-2'>
+              <button  type="button" className='delete-btn' id={`remove-lang-${index}`} onClick={() => handleRemoveLang(lang.language)}>
+                Remove {lang.language} language
+              </button>
+            </div>
+          </>
           )
         })}
         <button type='button' className='form-btn' onClick={handleAddNewLang}>
