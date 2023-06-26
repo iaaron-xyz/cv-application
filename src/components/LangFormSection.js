5c0f5b4b93c073c1523eb56cc1ce495769687e67
Aaron <iaaron.xyz@outlook.com>
2023-06-26 15:37:13 -0600

    edit language and level fields

--- src/components/LangFormSection.js
+++ src/components/LangFormSection.js
@@ -2,18 +2,34 @@ import React from 'react'
 
 const LangFormSection = ({ data, setData }) => {
 
+  const handleInputLang = (e) => {
+    // get index number and associated sub-field
+    const langId = parseInt(e.target.id.split("-").pop());
+    const langField = e.target.id.split("-")[0];
+    // temporal copy of skills list
+    const updatedLang = [...data.languages];
+    // update category
+    updatedLang[langId] = {
+      ...data.languages[langId],
+      [langField]: e.target.value
+    };
+    // update data
+    setData({...data, languages: updatedLang});
+  };
+  
   const handleAddNewLang = () => {
     setData({...data,
       languages: [...data.languages, {language: "", level: ""}]
     });
   }
+
   return (
     <div className='p-4'>
       <h2 className='text-2xl'>Languages</h2>
       <form>
         {data.languages.map((lang, index) => {
           return (
-            <div className='grid grid-cols-2 gap-1'>
+            <div key={index} className='grid grid-cols-2 gap-1'>
               <div className='form-element'>
                 <label>
                   Language
@@ -23,6 +39,7 @@ const LangFormSection = ({ data, setData }) => {
                   id={`language-${index}`}
                   className='block w-full'
                   value={lang.language}
+                  onChange={handleInputLang}
                 />
               </div>
               <div className='form-element'>
@@ -34,6 +51,7 @@ const LangFormSection = ({ data, setData }) => {
                 id={`level-${index}`}
                 className='block w-full'
                 value={lang.level}
+                onChange={handleInputLang}
               />
             </div>
           </div>
