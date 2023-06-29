b05e1a9a7ec8066302143feabc2a49c0e2416622
Aaron <iaaron.xyz@outlook.com>
2023-06-29 01:54:10 -0600

    style contact-info bar

--- src/components/RenderHeader.js
+++ src/components/RenderHeader.js
@@ -15,49 +15,70 @@ const HeaderRender = ({ data }) => {
         </p>
       </div>
       <div className="bg-cyan-950 py-2">
-        <ul className="mono-font list-none list-inline text-center text-xs">
-        <li>
-            { data.contactInfo.email ? (
-              <a href={data.contactInfo.email}>
-                {data.contactInfo.email}
-              </a>
-            ) : (
-              ''
-            )}
-          </li>
-          <li>
-            {data.contactInfo.number}
-          </li>
-          <li>
-            { data.contactInfo.linkedin ? (
-              <a href={data.contactInfo.linkedin}>
-                linkedIn
-              </a>
-            ) : (
-              ''
-            )}
-          </li>
-          <li>
-            { data.contactInfo.github ? (
-              <a href={data.contactInfo.github}>
-                Github
-              </a>
-            ) : (
-              ''
-            )}
-          </li>
-          <li>
-            { data.contactInfo.website ? (
-              <a href={data.contactInfo.website}>
-                My portfolio
-              </a>
-            ) : (
-              ''
-            )}
-          </li>
+        <ul className="mono-font flex justify-center items-center text-center text-xxs">
+          <ContactItems data={data} />
         </ul>
       </div>
     </div>
+  )
+}
+
+const ContactItems = ({data}) => {
+
+  
+  return (
+    <>
+      <li className={data.contactInfo.email && 'mx-3'}>
+        { data.contactInfo.email ? (
+          <a href={data.contactInfo.email}>
+            {data.contactInfo.email}
+          </a>
+        ) : (
+          ''
+        )}
+      </li>
+      <li className={data.contactInfo.number && 'mx-3'}>
+        {data.contactInfo.number}
+      </li>
+      <li className={data.contactInfo.linkedin && 'mx-3 flex flex-col items-start'}>
+        <ContactUserItem
+          site={'linkedin.com/in'}
+          username={data.contactInfo.linkedin}
+        />
+      </li>
+      <li className={data.contactInfo.github && 'mx-3 flex flex-col items-start'}>
+        <ContactUserItem
+          site={'github.com'}
+          username={data.contactInfo.github}
+        />
+      </li>
+      <li className={data.contactInfo.website && 'mx-3'}>
+        { data.contactInfo.website ? (
+          <a href={data.contactInfo.website}>
+            Website
+          </a>
+        ) : (
+          ''
+        )}
+      </li>
+    </>
+  )
+}
+
+const ContactUserItem = ({site, username}) => {
+  return (
+    <>
+      { username ? (
+        <>
+          <p className='text-xxxs text-teal-500'>{site}/</p>
+          <a href={`https://www.${site}/${username.split('/').at(-1) || username.split('/').at(-2)}`}>
+            {username.split('/').at(-1) || username.split('/').at(-2)}
+          </a>
+        </>
+      ) : (
+        ''
+      )}
+    </>
   )
 }
 
