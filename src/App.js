c6b10a1418ef8445bbe62afdda21093757c5395d
Aaron <iaaron.xyz@outlook.com>
2023-07-01 02:20:27 -0600

    add Navbar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import { useState } from "react";
 import EditSection from "./components/EditSection";
 import RenderSection from "./components/RenderSection";
 import { cvData } from "./components/cvData";
+import Navbar from "./components/Navbar";
 
 const App = () => {
 
   const [data, setData] = useState(cvData);
 
   return (
-    <div className="main-font p-4 flex flex-row justify-center flex-wrap bg-slate-300">
-      <EditSection data={data} setData={setData}/>
-      <div className="drop-shadow-xl">
-        <RenderSection data={data}/>
+    <>
+      <Navbar />
+
+      <div className="main-font p-4 flex flex-row justify-center flex-wrap bg-slate-300">
+        <EditSection data={data} setData={setData}/>
+        <div className="drop-shadow-xl">
+          <RenderSection data={data}/>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+const Navbar = () => {
+  return (
+    <div className="flex w-full bg-slate-900 text-white p-6">
+      <h1 className="main-font text-4xl">CV Creator</h1>
+      <button className="ml-auto p-2 bg-emerald-600 rounded-md">Generate PDF</button>
+    </div>
+  )
+}
+
+export default Navbar
